6591d134a9fa2500f3a345f2d73526b46981131e
PhamQuyen11 <167006526+PhamQuyen11@users.noreply.github.com>
2024-04-19 00:09:03 +0700

    Create Practice3.sql

---
+++ Practice3.sql
@@ -0,0 +1,53 @@
+--- ex1
+select Name from STUDENTS
+where Marks > 75 
+order by right (name, 3) asc, id
+--- ex2
+select user_id, 
+concat(upper(left(name,1)), lower(right(name,length(name)-1))) as name --- concat(upper(left(name,1)), lower(substring (name,2))) as name 
+from Users
+order by user_id 
+--- ex 3
+SELECT manufacturer,
+'$' || round (sum (total_sales)/1000000) ||' million' as sale 
+FROM pharmacy_sales
+group by manufacturer 
+order by sum (total_sales) desc, manufacturer
+--- ex4
+SELECT 
+extract (month from submit_date), 
+product_id as product,
+round (avg (stars),2) as avg_star 
+FROM reviews
+group by extract (month from submit_date), product 
+order by  extract (month from submit_date), product_id
+--- ex 5
+SELECT 
+sender_id,
+count(content) as message_count
+FROM messages
+where extract (month from sent_date) = '8' and extract (year from sent_date) = '2022'
+group by sender_id
+order by count(content) desc
+limit 2 
+--- ex6
+select 
+tweet_id from Tweets
+where length (content) > 15 
+--- ex7
+--- ex8
+select 
+count (employee_id) as number_employee
+from employees
+where extract (month from joining_date ) in ('1','2','3','4','5','6','7') 
+and extract (year from joining_date ) = '2022'
+--- ex9
+select 
+position ('a' in first_name) as pst
+from worker
+where first_name = 'Amitah'
+--- ex10
+select 
+cast (substring (title, position ('2' in title), 4) as int )
+from winemag_p2
+where country = 'Macedonia'
