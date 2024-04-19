c4fc07e47e339af628e7f59fbe796a659e1b5e9d
PhamQuyen11 <167006526+PhamQuyen11@users.noreply.github.com>
2024-04-19 07:42:34 +0700

    Update Practice3.sql

--- Practice3.sql
+++ Practice3.sql
@@ -35,6 +35,11 @@ select
 tweet_id from Tweets
 where length (content) > 15 
 --- ex7
+activity_date as day,
+count (distinct user_id) as active_users from Activity 
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+group by activity_date
+order by activity_date 
 --- ex8
 select 
 count (employee_id) as number_employee
